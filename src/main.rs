84acee44481abf949cec70dd3d4870e48f69ae0f
Thomas Muntaner <thomas.muntaner@gmail.com>
2022-03-14 16:03:19 +0100

    Started ssh agent

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+extern crate core;
+
 mod gpg;
 mod licenses;
+mod ssh;
 
 use crate::gpg::Gpg;
 use crate::licenses::Licenses;
-use anyhow::Result;
+use crate::ssh::Ssh;
+use anyhow::{anyhow, Result};
 use clap::Parser;
+use flexi_logger::{FileSpec, Logger, WriteMode};
 
 #[derive(Parser)]
 #[clap(
@@ -19,14 +24,23 @@ struct Opts {
 #[derive(Parser)]
 enum SubCommand {
     Gpg(Gpg),
+    Ssh(Ssh),
     Licenses(Licenses),
 }
 
 fn main() -> Result<()> {
+    let path = dirs::cache_dir()
+        .ok_or_else(|| anyhow!("could not determine config directory"))?
+        .join("wsl-gpg-agent");
+    let _logger = Logger::try_with_str("info")?
+        .log_to_file(FileSpec::default().suppress_timestamp().directory(path))
+        .write_mode(WriteMode::BufferAndFlush)
+        .start()?;
     let opt: Opts = Opts::parse();
 
     match opt.sub_command {
         SubCommand::Gpg(val) => val.run()?,
+        SubCommand::Ssh(val) => val.run()?,
         SubCommand::Licenses(val) => val.run()?,
     }
 
